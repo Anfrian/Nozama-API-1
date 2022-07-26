604258a395af21e6a8e5d5b567c91fcf2eda4486
Anfrian <gorkemkadir14@hotmail.com>
2022-07-26 03:07:34 +0300

    models

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
-const { application } = require('express');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -15,6 +15,7 @@ mongoose
 
 app.use(express.json());
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('Server running at http://localhost:5000');
