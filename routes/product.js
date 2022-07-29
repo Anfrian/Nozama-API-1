d8d2d9b8a87f93204718706031c56e1116afec6e
Anfrian <gorkemkadir14@hotmail.com>
2022-07-29 03:00:14 +0300

    Cart & Order

--- routes/product.js
+++ routes/product.js
@@ -23,7 +23,7 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
         const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {$set: req.body,}, {new : true});
         res.status(200).json(updatedProduct);
     } catch (err) {
-
+        res.status(500).json(err);
     }
 });
 
